b1818a9de508cbeadf2e815f1cd28a815288797a
Naod <chris_naod@yahoo.com>
2025-01-20 22:01:30 +1100

    generate random password

--- main.py
+++ main.py
@@ -9,14 +9,25 @@ from cryptography.fernet import Fernet
 from base64 import urlsafe_b64encode
 
 
- # Create key from master password
- def create_key(master_password: str, salt: bytes):
-     kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=100000,
-            backend=default_backend()
-     )
-     return urlsafe_b64encode(kdf.derive(master_password.encode()))
+# Create key from master password
+def create_key(master_password: str, salt: bytes):
+ kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+ )
+ return urlsafe_b64encode(kdf.derive(master_password.encode()))
+
+# Generate a strong random password
+def generate_password(length= 12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+
+
+
+
 
